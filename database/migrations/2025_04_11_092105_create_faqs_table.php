71e751a539f5bf3bcdb65109e8fbe63df83b2c85
Jose Brites <2221393@my.ipleiria.pt>
2025-05-21 13:22:28 +0100

    merge users-page to main

--- database/migrations/2025_04_11_092105_create_faqs_table.php
+++ database/migrations/2025_04_11_092105_create_faqs_table.php
@@ -10,7 +10,6 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-<<<<<<<< HEAD:database/migrations/2025_04_11_092105_create_faqs_table.php
     {
         Schema::create('faqs', function (Blueprint $table) {
             $table->id();
@@ -20,25 +19,11 @@ return new class extends Migration
         });
     }
  
-========
-{
-    Schema::table('users', function (Blueprint $table) {
-        $table->enum('tipo', ['admin', 'user'])->default('user')->after('email');
-    });
-}
-
->>>>>>>> users-page:database/migrations/2025_04_22_151720_add_tipo_to_users_table.php
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-<<<<<<<< HEAD:database/migrations/2025_04_11_092105_create_faqs_table.php
         Schema::dropIfExists('faqs');
-========
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('tipo');
-        });
->>>>>>>> users-page:database/migrations/2025_04_22_151720_add_tipo_to_users_table.php
     }
 };
--- database/migrations/2025_04_22_151720_add_tipo_to_users_table.php
+++ database/migrations/2025_04_22_151720_add_tipo_to_users_table.php
@@ -1,44 +1,28 @@
 <?php
- 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
- 
+
 return new class extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up(): void
-<<<<<<<< HEAD:database/migrations/2025_04_11_092105_create_faqs_table.php
     {
-        Schema::create('faqs', function (Blueprint $table) {
-            $table->id();
-            $table->string('pergunta');
-            $table->text('resposta');
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('tipo', ['admin', 'user'])->default('user')->after('email');
         });
     }
- 
-========
-{
-    Schema::table('users', function (Blueprint $table) {
-        $table->enum('tipo', ['admin', 'user'])->default('user')->after('email');
-    });
-}
 
->>>>>>>> users-page:database/migrations/2025_04_22_151720_add_tipo_to_users_table.php
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-<<<<<<<< HEAD:database/migrations/2025_04_11_092105_create_faqs_table.php
-        Schema::dropIfExists('faqs');
-========
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('tipo');
         });
->>>>>>>> users-page:database/migrations/2025_04_22_151720_add_tipo_to_users_table.php
     }
 };
