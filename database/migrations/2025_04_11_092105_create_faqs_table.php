45d6c3634339c4b06dd1b112f5c1b37501c3d919
Jose Brites <2221393@my.ipleiria.pt>
2025-05-21 13:13:34 +0100

    rotas na main

--- database/migrations/2025_04_11_092105_create_faqs_table.php
+++ database/migrations/2025_04_11_092105_create_faqs_table.php
@@ -1,9 +1,9 @@
 <?php
-
+ 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+ 
 return new class extends Migration
 {
     /**
@@ -11,30 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-<<<<<<<< HEAD:database/migrations/2025_04_11_092105_create_faqs_table.php
         Schema::create('faqs', function (Blueprint $table) {
             $table->id();
             $table->string('pergunta');
             $table->text('resposta');
-========
-        Schema::create('ponto_imagens', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('ponto_id')->constrained('pontos')->onDelete('cascade');
-            $table->string('caminho');
->>>>>>>> rotas:database/migrations/2025_05_05_220200_create_ponto_imagens_table.php
             $table->timestamps();
         });
     }
-
+ 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-<<<<<<<< HEAD:database/migrations/2025_04_11_092105_create_faqs_table.php
         Schema::dropIfExists('faqs');
-========
-        Schema::dropIfExists('ponto_imagens');
->>>>>>>> rotas:database/migrations/2025_05_05_220200_create_ponto_imagens_table.php
     }
 };
