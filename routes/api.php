db179a1a8be7e7b6b8a3543755a66771a4afbc75
Jose Brites <2221393@my.ipleiria.pt>
2025-07-03 13:57:34 +0100

    Correção de um pequeno erro

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\RotasController;
 use App\Http\Controllers\FaqController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PontoController;
 
 //registo
 Route::post('/signup', [RegisterController::class, 'storeApi']);
@@ -18,7 +19,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [LoginController::class, 'logoutApi']);
 
     Route::post('/deleteacc', [UserController::class, 'destroyApi']);
-    
+
     Route::put('/updatefirstname', [UserController::class, 'updateFirstName']);
     Route::put('/updatelastname', [UserController::class, 'updateLastName']);
     Route::put('/updateemail', [UserController::class, 'updateEmail']);
@@ -28,9 +29,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/rotas/search', [RotasController::class, 'indexApi']);
     Route::get('/rotas/{id}', [RotasController::class, 'getRotaApi']);
     Route::get('/rotas/zona/{zona}', [RotasController::class, 'getRotasByZona']);
-    
+
     Route::get('/faqs', [FaqController::class, 'indexApi']);
     Route::get('/faqs/{id}', [FaqController::class, 'showApi']);
+
+    
+    Route::post('/rotas/{rota}/pontos', [PontoController::class, 'storeApi']);
+    Route::get('/rotas/{rota}/pontos', [PontoController::class, 'indexApi']);
+    Route::get('/pontos/{id}', [PontoController::class, 'showApi']);
 });
-
-
